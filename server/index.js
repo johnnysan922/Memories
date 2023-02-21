09f37b135df302414437685b531cfb5bf4a3ce10
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-21 12:36:02 -0500

    Setup connection to mongoose

--- server/index.js
+++ server/index.js
@@ -3,4 +3,23 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
-const express = require('express');
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+// CONNECTION_URL obtained from mongodb
+// <password>
+const CONNECTION_URL = 'mongodb+srv://johnnyss1999:<password>@cluster0.vgseof5.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+// CONNECT TO MONGOOSE
+mongoose.set('strictQuery', false);
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
