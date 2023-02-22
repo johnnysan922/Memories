9548def9704bb3c2b9fc4cea865ad0ca9ca06fa7
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-21 19:58:46 -0500

    Removed <React.strictMode>

--- client/src/index.js
+++ client/src/index.js
@@ -12,11 +12,9 @@ const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
