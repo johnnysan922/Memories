d8ae856eeabfbe52f3efaecb132bfac59f48e447
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-28 09:50:45 -0500

    Redeployed on Render

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // TESTING
 // http://localhost:5000/posts
-const url = 'https://memories-7v4s.onrender.com/';
+const url = 'https://memories-7v4s.onrender.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
