0aa267f57133621203cb80e8c207a0c0062eb452
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-28 10:26:10 -0500

    Redeployed on Render

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,7 +5,7 @@ export const getPosts = async (req, res) => {
     try{
         const postMessages = await PostMessage.find();
 
-        console.log(postMessages);
+        // console.log(postMessages);
         res.status(200).json(postMessages);
 
     } catch(error) {
